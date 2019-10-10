894b6c28a88860fe51a33eb0c799de49d3bc25aa
Maria Paderina <mariapaderina79@gmail.com>
2019-10-10 13:12:15 -0700

    Initial Commit

---
+++ MakeupApiSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeupApiSwiftUI
+//
+//  Created by Maria Paderina on 10/10/19.
+//  Copyright © 2019 Maria Paderina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
